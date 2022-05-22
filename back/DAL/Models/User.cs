294fd7f0a24f36963510acb0f661ad38525982bc
Khantain <quentinjouhault@gmail.com>
2022-05-22 14:13:13 +0200

    ci: c# linter for back office

--- back/DAL/Models/User.cs
+++ back/DAL/Models/User.cs
@@ -1,7 +1,7 @@
 public class User
 {
     public int Id { get; set; }
-    public string Name { get; set; }  = null!;
+    public string Name { get; set; } = null!;
     public string Email { get; set; } = null!;
     public Status Status { get; set; } = null!;
 }
