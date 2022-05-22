6b6fd32876a83a4a7fb9df83a89164712b2c1b98
Khantain <quentinjouhault@gmail.com>
2022-05-22 13:55:43 +0200

    refactor: back in its folder

--- back/Context/DBContext.cs
+++ back/Context/DBContext.cs
@@ -4,5 +4,5 @@ public class DBContext : DbContext
 {
     public DBContext(DbContextOptions<DBContext> options) : base(options) { }
 
-    public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; } = null!;
 }
